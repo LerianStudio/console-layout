23f55993cd5cd7eb282974012b797aeb30093a07
Augusto Alvarenga <augusto.santesso@gmail.com>
2025-07-01 21:02:15 -0300

    feat: resolve css problems

--- test-app/next.config.js
+++ test-app/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@layout/layout"],
+  transpilePackages: ["@lerian/console-layout"],
 };
 
 module.exports = nextConfig;
