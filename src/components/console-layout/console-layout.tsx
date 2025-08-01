386a25ad9dc9449ab891c4f9926bb2bc74e70b25
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2025-08-01 14:34:04 -0300

    feat: Added permissions provider in component

--- src/components/console-layout/console-layout.tsx
+++ src/components/console-layout/console-layout.tsx
@@ -6,21 +6,24 @@ import { PageContent, PageRoot, PageView } from '../page'
 import { Sidebar } from '../sidebar'
 import { LocalizationProvider } from '@/lib/intl/localization-provider'
 import { QueryProvider } from '@/providers/query-provider'
+import { PermissionProvider } from '@/providers/permission-provider'
 
 export const ConsoleLayout = ({ children }: React.PropsWithChildren) => {
   return (
     <LocalizationProvider>
       <QueryProvider>
         <OrganizationProvider>
-          <SidebarProvider>
-            <PageRoot>
-              <Sidebar />
-              <PageView>
-                <Header />
-                <PageContent>{children}</PageContent>
-              </PageView>
-            </PageRoot>
-          </SidebarProvider>
+          <PermissionProvider>
+            <SidebarProvider>
+              <PageRoot>
+                <Sidebar />
+                <PageView>
+                  <Header />
+                  <PageContent>{children}</PageContent>
+                </PageView>
+              </PageRoot>
+            </SidebarProvider>
+          </PermissionProvider>
         </OrganizationProvider>
       </QueryProvider>
     </LocalizationProvider>
